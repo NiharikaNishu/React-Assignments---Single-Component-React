63da351cdaed427f8768975db8b29249e0f9da50
Niharika Nishu <46600925+NiharikaNishu@users.noreply.github.com>
2020-11-05 01:53:46 +0530

    cmmt

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,13 @@
 import React, { Component, useState } from "react";
 import "../styles/App.css";
 
+const element = (
+  <div id="root">
+    <p>I am learning React. My life is getting better.</p>
+  </div>
+);
+ReactDOM.render(element, document.getElementById("root"));
+
 const App = () => {
   return <></>;
 };
--- src/index.js
+++ src/index.js
@@ -2,9 +2,4 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
 
-const element = (
-  <div id="root">
-    <p>I am learning React. My life is getting better.</p>
-  </div>
-);
-ReactDOM.render(element, document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById("root"));
