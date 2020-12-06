0dab3486b7a8cc14d41c0ce35028d6269671186b
Niharika Nishu <46600925+NiharikaNishu@users.noreply.github.com>
2020-12-06 13:44:04 +0530

    change

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,16 @@
 import React, { Component, useState } from "react";
 import "../styles/App.css";
 
-const App = () => {
-  return (
-    <>
-      <p>I am learning React. My life is getting better. </p>
-    </>
-  );
-};
+// const App = () => {
+//   return (
+//     <>
+//       <p>I am learning React. My life is getting better. </p>
+//     </>
+//   );
+// };
+
+function App() {
+  return <p>I am learning React. My life is getting better.</p>;
+}
 
 export default App;
